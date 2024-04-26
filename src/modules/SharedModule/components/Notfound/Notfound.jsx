3a911fbebc85a572e19d90102ace26e8707f9c4d
essam74 <emostafa580@gmail.com>
2024-04-26 19:07:10 +0300

    not found page

--- src/modules/SharedModule/components/Notfound/Notfound.jsx
+++ src/modules/SharedModule/components/Notfound/Notfound.jsx
@@ -1,7 +1,45 @@
 // import React from 'react'
+import logo from "../../../../assets/images/logo.png";
+import notfound from "../../../../assets/images/Group 48101676.png";
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Notfound() {
+
+  const navigate = useNavigate()
+
+  const handleSubmit = ()=>{
+    navigate('/dashboard')
+
+  }
+
   return (
-    <div>Notfound</div>
+    <div className="notFound vh-100 ">
+      <div className="container-fluid ">
+        <div className="w-25 pt-3">
+            <img src={logo} alt="" className="w-100 " />
+        </div>
+
+        <div className="notfound-content ">
+              <h2>Oops..</h2>
+              <h3 className="text-success">Page not found</h3>
+              <p>This Page doesn’t exist or was removed! <br /> We suggest you back to home.</p>
+              
+              <button className="btn btn-success w-75 py-0 my-4" onClick={handleSubmit}>
+                      <div className="d-flex align-items-center justify-content-center py- ">
+                        <i className="fa-solid fa-arrow-left "></i>
+                        <p className="py-0 my-0 back">Back To <br /> Home</p>
+                      </div>
+              </button>
+
+              
+        </div>
+
+        <div className="notfound-img ">
+          <img src={notfound} alt="" className="w-100"/>
+        </div>
+      </div>
+      
+    </div>
   )
 }
